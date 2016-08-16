87fdba3654589e6e0610dd0d9d09b433b0a148ab
Howard <guohouzuo@gmail.com>
2016-08-16 09:24:40 +0200

    Initial commit

---
+++ cpsamples.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for i in {1..99}; do
+  cp sample-video.webm sample-video-$i.webm
+done
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU() * 6)
+	fmt.Println("gomaxprocs is set to", runtime.GOMAXPROCS(0))
+	rand.Seed(time.Now().UnixNano())
+	http.HandleFunc("/playerstat", func(w http.ResponseWriter, r *http.Request) {
+		// Print player's browser information to stdout
+		log.Printf("Player %s (%s x %s) is a '%s'", r.RemoteAddr, r.FormValue("width"), r.FormValue("height"), r.Header.Get("User-Agent"))
+	})
+	http.HandleFunc("/playbackstat", func(w http.ResponseWriter, r *http.Request) {
+		// Print playback information submitted by browser to stdout
+		log.Printf("Player %s is playing %s (%s x %s) at %s", r.RemoteAddr, r.FormValue("currenttype"), r.FormValue("width"), r.FormValue("height"), r.FormValue("currenttime"))
+	})
+	http.HandleFunc("/randvideo", func(w http.ResponseWriter, r *http.Request) {
+		// Serve a random video among 99 video files. cpsamples.sh makes the 99 copies.
+		http.ServeFile(w, r, "sample-video-"+strconv.Itoa(1+rand.Intn(98))+".mp4")
+	})
+	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	// Serve HTTPS on 10987 and HTTP on 10988
+	go func() {
+		if err := http.ListenAndServeTLS("0.0.0.0:10987", "tls.crt", "tls.key", nil); err != nil {
+			panic(err)
+		}
+	}()
+	if err := http.ListenAndServe("0.0.0.0:10988", nil); err != nil {
+		panic(err)
+	}
+}
